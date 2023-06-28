aa9899d75a359f3539277dd6fc9aa4fa6ca5b578
--global <anthony.anthony.hoanghoang@gmai.com>
2023-06-29 04:52:10 +0800

    regenerating wasm files

--- regenerate_wasm.sh
+++ regenerate_wasm.sh
@@ -7,7 +7,8 @@ set -o pipefail  # don't hide errors within pipes
 
 
 RUSTFLAGS=('' '' 'RUSTFLAGS="-C link-arg=--export-table" ' );
-FLAGS=('--target=wasm32-unknown-unknown' '--target=wasm32-unknown-unknown' '--target=wasm32-unknown-unknown');
+FLAGS=('--target=wasm32-unknown-unknown --profile small' '--target=wasm32-unknown-unknown' '--target=wasm32-unknown-unknown');
+BIN_LOCATION=('small' 'release' 'release')
 
 readonly RUSTFLAGS;
 readonly FLAGS;
@@ -16,10 +17,11 @@ readonly FLAGS;
 for i in "${!FLAGS[@]}"; do 
   flags="${FLAGS[$i]}";
   rust_flags="${RUSTFLAGS[$i]}";
+  bin_location="${BIN_LOCATION[$i]}";
 
   save_location="'./wat/${rust_flags}cargo build ${flags}.wat'";
   
-  exec_command="${rust_flags}cargo build ${flags} > ${save_location}";
+  exec_command="${rust_flags}cargo build ${flags}; wasm2wat ./target/wasm32-unknown-unknown/${bin_location}/wasm_playground.wasm > ${save_location}";
   echo "$exec_command";
   bash -c "${exec_command}";
 done
